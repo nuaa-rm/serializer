b2f5aa7b61767c602339479cb77ea92f363a8538
bismarck <zuoqingyu@nuaa.edu.cn>
2022-07-28 18:48:59 +0800

    [feature] 增加c端环形缓冲区模块

---
+++ exampleRingBuf.c
@@ -0,0 +1,19 @@
+//
+// Created by bismarck on 22-7-28.
+//
+#include "ringBuffer.h"
+#include <stdio.h>
+
+int main() {
+    ringBuffer_t buf = createRingBuffer(10);
+    char output[100] = {0};
+
+    buf->put(buf, "0123456789", 10);
+    buf->get(buf, output, 10);
+    printf("%s\n", output);
+
+    buf->pop(buf, 4);
+    buf->put(buf, "1\0003", 4);
+    buf->get(buf, output, 10);
+    printf("%s\n", output);
+}
--- ringBuffer.h
+++ ringBuffer.h
@@ -137,6 +137,103 @@ public:
 };
 #else
 
+#include <string.h>
+#include <stdlib.h>
+
+struct ringBuffer {
+    uint8_t* buffer;
+    uint8_t* head;
+    uint8_t* tail;
+    int length;
+    int (*size)(struct ringBuffer* this);
+    int (*get)(struct ringBuffer* this, void* dst, const int size);
+    int (*pop)(struct ringBuffer* this, const int size);
+    int (*put)(struct ringBuffer* this, const void* src, const int size);
+    void (*deinit)(struct ringBuffer* this);
+};
+typedef struct ringBuffer* ringBuffer_t;
+
+int rb_outOfSize(ringBuffer_t this) {
+    return this->tail < this->head;
+}
+
+int rb_remainingSpace(ringBuffer_t this) {
+    return this->length - this->size(this);
+}
+
+int rb_get(ringBuffer_t this, void* dst, const int size) {
+    if (size > this->size(this)) {
+        return 0;
+    }
+    if (rb_outOfSize(this)) {
+        int head2size = this->length - (int)(this->head - this->buffer) + 1;
+        memcpy(dst, this->head, head2size);
+        memcpy((uint8_t*)dst + head2size, this->buffer, (int)(this->tail - this->buffer));
+    } else {
+        memcpy(dst, this->head, (int)(this->tail - this->head));
+    }
+    return 1;
+}
+
+int rb_pop(ringBuffer_t this, const int size) {
+    if (size > this->size(this)) {
+        return 0;
+    }
+    int head2size = this->length - (int)(this->head - this->buffer) + 1;
+    if (size >= head2size) {
+        this->head = this->buffer + size - head2size;
+    } else {
+        this->head += size;
+    }
+    return 1;
+}
+
+int rb_put(ringBuffer_t this, const void* src, const int size) {
+    if (size > rb_remainingSpace(this)) {
+        return 0;
+    }
+    int tail2size = this->length - (int)(this->tail - this->buffer) + 1;
+    if (size > tail2size) {
+        memcpy(this->tail, src, tail2size);
+        memcpy(this->buffer, (uint8_t*)src + tail2size, size - tail2size);
+        this->tail = this->buffer + size - tail2size;
+    } else if (size == tail2size) {
+        memcpy(this->tail, src, tail2size);
+        this->tail = this->buffer;
+    } else {
+        memcpy(this->tail, src, size);
+        this->tail += size;
+    }
+    return 1;
+}
+
+int rb_size(ringBuffer_t this) {
+    if (rb_outOfSize(this)) {
+        return this->length - (int)(this->head - this->tail) + 1;
+    } else {
+        return (int)(this->tail - this->head);
+    }
+}
+
+void rb_deinit(ringBuffer_t this) {
+    free(this->buffer);
+    free(this);
+}
+
+ringBuffer_t createRingBuffer(const int size) {
+    ringBuffer_t this = malloc(sizeof(struct ringBuffer));
+    this->length = size;
+    this->buffer = malloc(sizeof(uint8_t) * (size + 1));
+    this->head = this->buffer;
+    this->tail = this->buffer;
+    this->size = &rb_size;
+    this->get = &rb_get;
+    this->pop = &rb_pop;
+    this->put = &rb_put;
+    this->deinit = &rb_deinit;
+    return this;
+}
+
 #endif
 
 
